805d9c3c80f340637ec47d647c3ba60b3589f34a
AhmedFattahSyd <ahmed.fattah@gmail.com>
2021-01-20 14:38:42 +1100

    Alpha3

--- src/GraphApp.tsx
+++ src/GraphApp.tsx
@@ -59,7 +59,7 @@ enum MessageType {
 }
 
 export default class GraphApp extends React.Component<AppProps, AppState> {
-  private appVersion = "My Graph - Version: Alpha (0.39.2) - 11 Jan 2021";
+  private appVersion = "My Graph - Version: Alpha (0.39.3) - 20 Jan 2021";
   private maxDisplayWidth = 10000;
   private displayWidth: number = 3000;
   private graphExplorer: Explorer;
